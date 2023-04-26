843b7618ca426a0b59104a25696de5ddf9aea2f5
Valdemir Filho <filhux@gmail.com>
2023-04-26 16:02:52 -0300

    fix text after page reload

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.action.setBadgeText({
-    text: "on",
+    text: "off",
   });
 });
 
---
+++ script.js
@@ -0,0 +1,3 @@
+window.addEventListener('load', function () {
+  console.log('Rocketseat Resize Video loaded!');
+});
